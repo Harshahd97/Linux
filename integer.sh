745c21667c974751d6e57276a859422cc52ff129
Harshahd97 <harshahd839@gmail.com>
2024-01-06 06:29:12 +0000

    variable changed

--- integer.sh
+++ integer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Enter the integer value1:"
-read num1
+read a
 echo "Enter the integer value2:"
 read num2
 echo "Enter the integer value3:"
 read num3
-echo "The values entered are $num1 $num2 $num3"
+echo "The values entered are $a $num2 $num3"
